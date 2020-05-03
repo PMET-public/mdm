1c370353c0644ecfb7b02c688967cadda6d4af93
Keith Bentrup <keith@keithbentrup.com>
2020-05-03 18:42:22 -0400

    updates

--- bin/uninstall-initial-dependencies.sh
+++ bin/uninstall-initial-dependencies.sh
@@ -5,13 +5,13 @@ set -x # turn on debugging
 read -r -p $'\033[0;31mThis script is for testing on new systems only and should not ordinarily be run. ARE YOU SURE?? (y/n) \033[0m'
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Removing magento-cloud CLI"
-  rm -rf ~/.magento-cloud || :
+  rm -rf ~/.magento-cloud
   echo "Removing homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)" || :
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
   echo "Removing xcode tools"
-  sudo rm -rf /Library/Developer/CommandLineTools || :
+  sudo rm -rf /Library/Developer/CommandLineTools
   echo "Removing docker"
-  rm -rf /Applications/Docker.app || :
+  rm -rf /Applications/Docker.app
   echo "Removing MDM"
   rm -rf ~/.mdm
 fi
