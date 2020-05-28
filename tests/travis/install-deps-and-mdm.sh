33273f0dee874f28b8bb3b66b4649f12dd408b2c
Keith Bentrup <keith@keithbentrup.com>
2020-05-28 17:19:31 -0400

    test with homebrew removed

--- tests/travis/install-deps-and-mdm.sh
+++ tests/travis/install-deps-and-mdm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set +x
+shopt -s nocasematch
 
 # shellcheck source=../../bin/lib.sh
 source ./bin/lib.sh
 
 msg "Running launcher
 "
-output=$(./bin/launcher)
-msg "Output from launcher:
-$output"
+output="$(./bin/launcher)"
+[[ $output =~ install ]] || exit 1
 
 msg "Running launcher with output as param
 "
@@ -17,4 +17,21 @@ msg "Running launcher with output as param
 
 msg "Running launcher with dependencies now installed
 "
-./bin/launcher
+output2="$(./bin/launcher)"
+
+
+msg "Removing homebrew
+"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
+
+msg "Rerunning launcher
+"
+output3="$(./bin/launcher)"
+
+[[ "$output" -eq "$output3" ]] || exit 1
+
+msg "Rerunning launcher with new output
+"
+./bin/launcher "$output3"
+
+exit 0
