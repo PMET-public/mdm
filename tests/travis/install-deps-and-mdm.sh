de9efe06cc2a457322b6ca00d7488a5856c2a31b
Keith Bentrup <keith@keithbentrup.com>
2020-05-28 16:23:56 -0400

    don't require docker for travis

--- tests/travis/install-deps-and-mdm.sh
+++ tests/travis/install-deps-and-mdm.sh
@@ -5,9 +5,12 @@ set +x
 # shellcheck source=../../bin/lib.sh
 source ./bin/lib.sh
 
-msg "Running launcher ..."
+msg "Running launcher"
 output=$(./bin/launcher)
 msg "Output from launcher: $output"
 
-msg "Running launcher with output ..."
+msg "Running launcher with output as param"
 ./bin/launcher "$output"
+
+msg "Running launcher with dependencies now installed"
+./bin/launcher
