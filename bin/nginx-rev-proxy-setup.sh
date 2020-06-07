6b35824c5e2482f530c0943117588906ad109db2
Keith Bentrup <keith@keithbentrup.com>
2020-06-07 16:01:34 -0400

    do not map host ports; run on same docker network

--- bin/nginx-rev-proxy-setup.sh
+++ bin/nginx-rev-proxy-setup.sh
@@ -34,10 +34,10 @@ EOF
 done
 
 # ensure latest
-docker_nginx_image=$(pmetpublic/nginx-with-pagespeed:1.0)
+docker_nginx_image="pmetpublic/nginx-with-pagespeed:1.0"
 docker pull "$docker_nginx_image"
 
-cid=$(docker create --label mdm-nginx-rev-proxy -v "$mdm_cert_dir":/etc/letsencrypt -p 443:443 -p 80:80 "$docker_nginx_image")
+cid=$(docker create --label mdm-nginx-rev-proxy -v "$mdm_cert_dir":/etc/letsencrypt -p 443:443 -p 80:80 --network mdm_default "$docker_nginx_image")
 docker cp $tmp_dir/. $cid:/etc/nginx/conf.d
 # include pwa config too
 docker cp "$mdm_path/current/etc/nginx/conf.d/pwa.nginx.conf" $cid:/etc/nginx/conf.d
