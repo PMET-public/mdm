7ccf278b1776d762e9ef820ccee6b81804cbf49b
Keith Bentrup <keith@keithbentrup.com>
2020-06-08 17:26:00 -0400

    add some docker tests

--- tests/CI/docker-menu-selections.sh
+++ tests/CI/docker-menu-selections.sh
@@ -5,4 +5,13 @@ source ./bin/lib.sh
 
 [[ $debug ]] && set -x
 
+yes 'no' | ./bin/launcher rm_magento_docker_images
+yes | ./bin/launcher rm_magento_docker_images
+
+yes 'no' | ./bin/launcher reset_docker
+yes | ./bin/launcher reset_docker
+
+yes 'no' | ./bin/launcher wipe_docker
+yes | ./bin/launcher wipe_docker
+
 exit 0
