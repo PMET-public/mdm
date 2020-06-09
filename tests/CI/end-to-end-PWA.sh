a72d676cf7a218be1b1cafc396a55cbad15e40af
Keith Bentrup <keith@keithbentrup.com>
2020-06-09 19:31:44 -0400

    test opening MDM

--- tests/CI/end-to-end-PWA.sh
+++ tests/CI/end-to-end-PWA.sh
@@ -8,7 +8,9 @@ source ./bin/lib.sh
 
 is_mac && {
   ./bin/dockerize -s
+  open -a MDM
+  ps_output="$(ps aux | grep "MDM$")"
+  [[ $ps_output =~ Contents/MacOS/MDM$ ]] || error "Expected MDM process not found."
 }
-
 
 exit 0
