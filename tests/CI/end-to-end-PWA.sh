bd5317a0741ce42278b04016510ddc07dd90b60f
Keith Bentrup <keith@keithbentrup.com>
2020-06-10 09:33:13 -0400

    grep exit status prematurely exits if MDM did not start up

--- tests/CI/end-to-end-PWA.sh
+++ tests/CI/end-to-end-PWA.sh
@@ -9,7 +9,7 @@ source ./bin/lib.sh
 is_mac && {
   ./bin/dockerize -s
   # open -a MDM
-  ps_output="$(ps aux | grep "MDM$")"
+  ps_output="$(ps aux | grep "MDM$" || :)"
   [[ $ps_output =~ Contents/MacOS/MDM$ ]] || error "Expected MDM process not found."
 }
 
