46edc2a7855eaa97466d7031a9fbbccd14f8b2a8
Keith Bentrup <keith@keithbentrup.com>
2020-06-12 08:37:48 -0400

    clone our own repo for test

--- tests/CI/create-mdm-app-from-mc-repo.sh
+++ tests/CI/create-mdm-app-from-mc-repo.sh
@@ -7,7 +7,7 @@ set -e
 source ./bin/lib.sh
 
 if ! is_mac; then
-  ./bin/dockerize -g https://github.com/magento/magento-cloud.git -b 2.3.5
+  ./bin/dockerize -g https://github.com/PMET-public/magento-cloud.git -b ref-GA
   open -a MDM
   ps_output="$(ps aux | grep "MDM$" || :)"
   [[ $ps_output =~ Contents/MacOS/MDM$ ]] || error "Expected MDM process not found."
