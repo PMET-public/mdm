d3c2322f9e668aed8f20cb8999ebc37321be3f47
Keith Bentrup <keith@keithbentrup.com>
2020-07-29 07:29:54 -0400

    fix name in test

--- tests/CI/create-detached-mdm.sh
+++ tests/CI/create-detached-mdm.sh
@@ -8,8 +8,8 @@ source ./bin/lib.sh
 
 if is_mac; then
   ./bin/dockerize -d
-  open -a MDM
-  ps_output="$(ps aux | grep "MDM$" || :)"
+  open -a "$detached_project_name"
+  ps_output="$(ps aux | grep "$detached_project_name" || :)"
   [[ $ps_output =~ Contents/MacOS/MDM$ ]] || error "Expected MDM process not found."
 else
   warning_w_newlines "Test skipped."
