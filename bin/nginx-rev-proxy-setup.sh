f5fe560c6a18a2df105985001758d97745aa24ab
Keith Bentrup <keith@keithbentrup.com>
2020-08-09 10:47:27 -0400

    nginx needs larger default buffer to handle magento large response

--- bin/nginx-rev-proxy-setup.sh
+++ bin/nginx-rev-proxy-setup.sh
@@ -52,6 +52,9 @@ write_nginx_config_for_host_at_port() {
         proxy_pass http://$host_docker_internal:$web_port;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_buffer_size          128k;
+        proxy_buffers              4 256k;
+        proxy_busy_buffers_size    256k;
       }
     }
 EOF
