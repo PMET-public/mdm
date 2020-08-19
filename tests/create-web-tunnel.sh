ef123bae6e3a37a02bdbcfa74fd0bf54b25b5ba7
Keith Bentrup <keith@keithbentrup.com>
2020-08-19 09:38:57 -0400

    testing

--- tests/create-web-tunnel.sh
+++ tests/create-web-tunnel.sh
@@ -3,14 +3,17 @@
 set -e
 # set -x
 
-# shellcheck source=lib.sh
-source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../bin/lib.sh" || :
-
 echo "$TUNNEL_SSH_PK" > "$HOME/pk"
 
 remote_port="$(( $RANDOM + 20000 ))"
-network="$(find_bridged_docker_networks)"
-local_port="$(find_varnish_port_by_network "$network")"
+if [[ "$TUNNEL_LOCAL_PORT" =~ ^[0-9]+$ ]]; then
+  local_port="$TUNNEL_LOCAL_PORT"
+elif [[ "$TUNNEL_DOCKER_NAME_FILTER" ]]; then
+  local_port="$(docker ps -a --filter "name=$TUNNEL_DOCKER_NAME_FILTER" --format "{{.Ports}}" | perl -pe 's/.*://;s/-.*//')"
+fi
+
+[[ ! "$local_port" =~ ^[0-9]+$ ]] && echo "Could not find valid local port" && exit 1
 
 ssh -i "$HOME/pk" -NR "$remote_port":127.0.0.1:"$local_port" "$TUNNEL_SSH_URL"
 
+sleep 7200
