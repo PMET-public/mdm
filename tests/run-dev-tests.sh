f88a9eac5e130b37bf36a74e08a2aa20f890af7d
Keith Bentrup <keith@keithbentrup.com>
2020-08-26 13:47:29 -0400

    add script for dev to run tests in a reasonable order

---
+++ tests/run-dev-tests.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+# set -x
+
+this_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+tests=($(find "$this_dir" -type f -not -path "*/libs/*" -name "*.bats" | sort))
+deferred_tests=()
+
+for test in "${tests[@]}"; do
+  # defer sudo tests until end b/c will have to enter password multiple times
+  [[ "$test" =~ sudo ]] && deferred_tests+=("$test") && continue
+
+  "$this_dir/libs/bats/bin/bats" -T "$test"
+done
+
+for test in "${deferred_tests[@]}"; do
+  echo "$this_dir/libs/bats/bin/bats" -T "$test"
+done
