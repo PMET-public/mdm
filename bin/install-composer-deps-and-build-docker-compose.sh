15724037e5251dcd9857cf56eb0808a5d983d1b6
Keith Bentrup <keith@keithbentrup.com>
2020-08-31 18:08:58 -0400

    add ability to override mcd image versions

--- bin/install-composer-deps-and-build-docker-compose.sh
+++ bin/install-composer-deps-and-build-docker-compose.sh
@@ -11,3 +11,8 @@ grep -q 'pmet-public/magento-cloud-docker' composer.json ||
 composer require magento/magento-cloud-docker:dev-develop --no-suggest --no-ansi --no-interaction --no-progress
 
 ./vendor/bin/ece-docker build:compose --host="$app_hostname" --port=8080
+
+[[ "$OVERRIDE_MCD_IMAGE_VERSION" ]] && {
+  echo "Overriding docker-compose.yml image versions ..."
+  perl -pe "/image.*pmetpublic\// and s/-[a-f0-9]{7}'/-$OVERRIDE_MCD_IMAGE_VERSION'/" docker-compose.yml
+}
