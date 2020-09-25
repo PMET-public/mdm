c094ef0ed8ad5b1bc965435be97a4b69714a1849
Keith Bentrup <keith@keithbentrup.com>
2020-09-25 09:37:37 -0400

    align options for DRY code whether bundling or skipping

--- bin/install-composer-deps-and-build-docker-compose.sh
+++ bin/install-composer-deps-and-build-docker-compose.sh
@@ -13,9 +13,3 @@ composer require magento/magento-cloud-docker:dev-develop --no-suggest --no-ansi
 # creates docker-compose.yml & .docker/config.env w/ base64 encoded vals for the "generic" service extended by others
 # inspect w/ perl -MMIME::Base64 -ne '/(MAGENTO_CLOUD_.*?)=(.*)/ and print "\"$1\":".decode_base64($2).",\n"' .docker/config.env | perl -0777 -pe 's/^/{/;s/.$/}/;' | jq
 ./vendor/bin/ece-docker build:compose --host="$app_hostname"
-
-# OVERRIDE_MCD_IMAGE_VERSION is used to test new images from the pmetpublic/magento-cloud-docker project forked from magento
-[[ "$OVERRIDE_MCD_IMAGE_VERSION" ]] && {
-  echo "Overriding docker-compose.yml image versions ..."
-  perl -i -pe "/image.*pmetpublic\// and s/-[a-f0-9]{7}'/-$OVERRIDE_MCD_IMAGE_VERSION'/" docker-compose.yml
-} || :
