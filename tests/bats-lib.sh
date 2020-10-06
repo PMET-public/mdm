83735c24eb3d5be4fb569c9598b032500fb543dc
Keith Bentrup <keith@keithbentrup.com>
2020-10-06 13:24:20 -0400

    test lib update

--- tests/bats-lib.sh
+++ tests/bats-lib.sh
@@ -3,18 +3,30 @@
 set -e
 # set -x
 
+# get the most recently created app dir
+get_most_recent_mdm_app() {
+  ls -dtr "$HOME/Downloads/"*.app | tail -1 || :
+}
+
 post_magento_install_setup() {
+  local mdm_app_dir link_prefix="mdm-app-"
+
   shopt -s nocasematch
-  # get the most recently created app dir
-  app_dir="$(ls -dtr "$HOME"/Downloads/*.app | tail -1 || :)"
-  if [[ "$app_dir" ]]; then
-    app_link_name="mdm-app-$(echo "$app_dir" | perl -ne '/Downloads\/(.*)\.app/ and print $1')"
-    [[ -L "$app_link_name" ]] || {
-      ln -sfn "$app_dir/Contents/Resources/script" "$app_link_name"
-      ln -sfn "$app_dir/Contents/Resources/app" "$app_link_name-dir"
-      if [[ "$GITHUB_WORKSPACE" ]]; then
-        ln -sfn "$GITHUB_WORKSPACE" "$app_dir/Contents/Resources/app/mdm-dir"
-      fi
-    }
+
+  # create links if they have not already been created
+  if ! ls "$link_prefix*" > /dev/null 2>&1; then
+    mdm_app_dir="$(get_most_recent_mdm_app)"
+    [[ "$mdm_app_dir" ]] && {
+      app_link_name="mdm-app-$(echo "$mdm_app_dir" | perl -ne '/Downloads\/(.*)\.app/ and print $1')"
+      [[ -L "$app_link_name" ]] || {
+        ln -sfn "$mdm_app_dir/Contents/Resources/script" "$app_link_name"
+        [[ -d "$mdm_app_dir/Contents/Resources/app" ]] && {
+          ln -sfn "$mdm_app_dir/Contents/Resources/app" "$app_link_name-dir"
+          [[ "$GITHUB_WORKSPACE" ]] && {
+            ln -sfn "$GITHUB_WORKSPACE" "$mdm_app_dir/Contents/Resources/app/mdm-dir"
+          }
+        }
+      }
+    } || :
   fi
 }
