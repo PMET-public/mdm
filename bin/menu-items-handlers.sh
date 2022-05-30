42a0bed026d3c14660e477d569df7e0f26ef9c6b
Keith Bentrup <keith@keithbentrup.com>
2022-05-30 17:13:44 -0400

    better url output

--- bin/menu-items-handlers.sh
+++ bin/menu-items-handlers.sh
@@ -267,7 +267,7 @@ open_app() {
   if is_mac; then
     open "$url"
   else
-    curl --write-out 'Opening app url: %{http_code} %{url}' --silent --output /dev/null -L "$url"
+    curl --write-out 'Opening app url: %{http_code} %{url_effective}' --silent --output /dev/null -L "$url"
   fi
 }
 
